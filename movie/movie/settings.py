c11822a3b4f32b21dc93da595b56056dc761e883
Mudit Gupta <muditagarwal1029@gmail.com>
2023-06-21 11:31:38 +0530

    Just Checking - All Good

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -32,8 +32,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '' 
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'webmanager201@gmail.com' 
+EMAIL_HOST_PASSWORD = 'mitcvxwlfgxkcbqg'
 
 
 # Application definition
